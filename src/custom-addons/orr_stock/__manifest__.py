61dc711339293f12fb91ecb88acd199da204a6b1
troublero <tgeorge@orrcorp.com>
2020-09-14 16:42:42 -0400

    thom version of orr_stock

---
+++ src/custom-addons/orr_stock/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':'orr stock',
+    'description':'update to sales view',
+    'author':'Thomas George',
+    'depends':['sale','contacts'],
+    'data':['views/product.xml'],
+    'application':False,
+    'installable':True,
+
+}
