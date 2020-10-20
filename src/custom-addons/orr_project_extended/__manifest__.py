635804aca7fd2aa5e522ba36f7488bc26854531a
scampbell <scampbell@opensourceintegrators.com>
2020-10-20 10:50:03 -0700

    [IMP] Steve Updates

--- src/custom-addons/orr_project_extended/__manifest__.py
+++ src/custom-addons/orr_project_extended/__manifest__.py
@@ -1,14 +1,16 @@
 {
-    "name": "Orr Custom Addons",
-    "version": "1.0.0.0.0",
+    "name": "Orr Project Extended",
+    "version": "12.0.0.1",
     "license": "AGPL-3",
     "summary": "",
     "author": "Orr Cloud Dev Team trg",
     "maintainer": "Orr Cloud Dev Team",
     "website": "www.orrcorp.com",
     "category": "Projects",
-    "depends": [ 'fieldservice',
-        'project'],
+    "depends": [
+        'fieldservice',
+        'project',
+        'account_accountant'],
     "data": [
         "views/fsm_order_form_extended.xml",
         "views/project_task_form_extended.xml",
--- src/custom-addons/orr_project_extended/models/fsm_order.py
+++ src/custom-addons/orr_project_extended/models/fsm_order.py
@@ -1,9 +1,7 @@
-from odoo import api, fields, models
+from odoo import fields, models
+
 
 class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
-    accounting_tag = fields.Many2many(
-        'account.analytic.tag',readonly=True
-        
-    )
+    accounting_tag = fields.Many2many('account.analytic.tag', readonly=True)
