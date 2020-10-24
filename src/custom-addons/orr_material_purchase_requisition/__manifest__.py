000e34880827ecb7ce9ecd4e4f0d5e0a27208357
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2020-10-23 18:59:31 -0700

    [FIX]TEST Release support

--- src/custom-addons/orr_material_purchase_requisition/__manifest__.py
+++ src/custom-addons/orr_material_purchase_requisition/__manifest__.py
@@ -3,7 +3,7 @@
     "description": "orr custom update to material requistions",
     "author": "Thomas George",
     "depends": ["purchase", "material_purchase_requisitions"],
-    "data": ["views/material_req.xml"],
+    "data": ["views/material_requisition.xml"],
     "application": False,
     "installable": True,
 }
